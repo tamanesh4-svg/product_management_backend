d3a6e5dfb25f6c814c6e21670a94087c2a552d3d
tamanesh4-svg <tamanesh4@gmail.com>
2026-09-07 11:13:17 +0530

    cors manageed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const productRoutes = require("./src/routes/product.routes")
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://project-management-frontend-tau-tan.vercel.app/","http://localhost:5173",],
     credentials: true
 }))
 connectDB();
